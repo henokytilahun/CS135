08b5c75e6605110962703d32189b6ffc9e0f3d7c
henokytilahun <henokytilahun@gmail.com>
2022-10-01 17:43:48 -0700

    Finally got gender working (SwitchCase)

--- Assignments/Assignment_3/main.cpp
+++ Assignments/Assignment_3/main.cpp
@@ -1,119 +1,108 @@
 /*
 Place your header comment here
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void heart_display()
 {
-    cout<<endl
-    <<".@  @, (&           .(%(.               *#(,              .   \n"
-    <<"(@  @( &@      .@@@@@@@@@@@@@#     @@@@@@@@@@@@@#        @@@. \n"
-    <<"(@  @( &@    /@@@@@@@@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@    .@@@@@,\n"
-    <<"(@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   .@@@@@,\n"
-    <<"(@@@@@@@@   @@@@@@@@@@@@@@@@  .@@@@@@@@@@@@@@@@@@@@@(  .@@@@@,\n"
-    <<" @@@@@@@#   @@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@@@@@/  .@@@@@,\n"
-    <<"    @@.     &@@@@@@@@@@@.   @@   @@@@@@@@@@@@@@@@@@@   .@@@@@,\n"
-    <<"    @@.                   @@@@@   @@@@                 .@@@@@,\n"
-    <<"    @@.       #@@@@@@@@@@@@@@@@@   &#  .@@@@@@@@@@     .@@@@@,\n"
-    <<"    @@.         @@@@@@@@@@@@@@@@@.    %@@@@@@@@@#         ,@@,\n"
-    <<"    @@.           @@@@@@@@@@@@@@@@,  @@@@@@@@@%           ,@@.\n"
-    <<"    @@.             %@@@@@@@@@@@@@@@@@@@@@@@.             ,@@,\n"
-    <<"    @@.                %@@@@@@@@@@@@@@@@@,                ,@@,\n"
-    <<"    @@.                    &@@@@@@@@@,                    ,@@,\n"
-    <<"    @@.                     &@@@@@,                       ,@@,\n"
-    <<endl
-    <<"             Welcome to the Calorie Calculator App\n";
+    cout << endl
+         << ".@  @, (&           .(%(.               *#(,              .   \n"
+         << "(@  @( &@      .@@@@@@@@@@@@@#     @@@@@@@@@@@@@#        @@@. \n"
+         << "(@  @( &@    /@@@@@@@@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@    .@@@@@,\n"
+         << "(@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   .@@@@@,\n"
+         << "(@@@@@@@@   @@@@@@@@@@@@@@@@  .@@@@@@@@@@@@@@@@@@@@@(  .@@@@@,\n"
+         << " @@@@@@@#   @@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@@@@@/  .@@@@@,\n"
+         << "    @@.     &@@@@@@@@@@@.   @@   @@@@@@@@@@@@@@@@@@@   .@@@@@,\n"
+         << "    @@.                   @@@@@   @@@@                 .@@@@@,\n"
+         << "    @@.       #@@@@@@@@@@@@@@@@@   &#  .@@@@@@@@@@     .@@@@@,\n"
+         << "    @@.         @@@@@@@@@@@@@@@@@.    %@@@@@@@@@#         ,@@,\n"
+         << "    @@.           @@@@@@@@@@@@@@@@,  @@@@@@@@@%           ,@@.\n"
+         << "    @@.             %@@@@@@@@@@@@@@@@@@@@@@@.             ,@@,\n"
+         << "    @@.                %@@@@@@@@@@@@@@@@@,                ,@@,\n"
+         << "    @@.                    &@@@@@@@@@,                    ,@@,\n"
+         << "    @@.                     &@@@@@,                       ,@@,\n"
+         << endl
+         << "             Welcome to the Calorie Calculator App\n";
 }
 
+int main()
+{
 
-
-int main(){
-
-    //initalize global variables
+    // initalize global variables
     int age = 10;
     int weight = 50;
     int height = 40;
     char sex = 'M';
     int act_lvl = 0;
 
-    //ask user for age
+    // ask user for age
     cout << "To get started, enter your age in years:\n";
-    do
+    cin >> age;
+    if (cin.fail())
     {
-      if(cin.fail())
-      {
         cout << "Invalid Age!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      } else if (age < 10 || age > 79)
-      {
+        return 0;
+    }
+    else if (age < 10 || age > 79)
+    {
         cout << "Age must be between 10 and 79 years!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      }
-      cin >> age;
-    } while (cin.fail() || age < 10 || age > 79);
+        return 0;
+    }
 
-    //ask user for weight
+    // ask user for weight
     cout << "Enter weight in lbs:\n";
-    do
+    cin >> weight;
+    if (cin.fail())
     {
-      if(cin.fail())
-      {
         cout << "Invalid weight!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      } else if(weight < 50 || weight > 400)
-      {
+        return 0;
+    }
+    else if (weight < 50 || weight > 400)
+    {
         cout << "Weight must be between 50 lbs and 400 lbs!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      }
-      cin >> weight;
-    } while (cin.fail() || weight < 50 || weight > 400);
+        return 0;
+    }
 
-
-    //ask user for height
+    // ask user for height
     cout << "Enter height in inches:\n";
+    cin >> height;
 
-    do
+    if (cin.fail())
     {
-      if(cin.fail())
-      {
         cout << "Invalid height!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      } else if(height < 40 || height > 90)
-      {
-        cout << "Heigh must be between 40\" and 90\"!\n";
-        cin.clear();
-        cin.ignore(100,'\n');
-      }
-      cin >> height;
-    } while (cin.fail() || height < 40 || height > 90);
-
-    //ask for user Gender
-    cout << "Enter 'M' for male or 'F' for female:\n";
-    do
+        return 0;
+    }
+    else if (height < 40 || height > 90)
     {
-        if(cin.fail())
-        {
-            cout << "Invalid Entry!\n";
-            cin.clear();
-            cin.ignore(100,'\n');
-        } else if((sex!='M')||(sex!='m')||(sex!='F')||(sex!='f'))
-        {
-            cout << "Invalid Entry!\n";
-            cin.clear();
-            cin.ignore(100,'\n');
-        }
-        cin >> sex;
+        cout << "Heigh must be between 40\" and 90\"!\n";
+        return 0;
+    }
+
+    // ask for user Gender
+    cout << "Enter 'M' for male or 'F' for female:\n";
+    cin >> sex;
+
+    switch(sex)
+    {
+        case 'M':
+            sex = 'M';
+            break;
+        case 'm':
+            sex = 'm';
+            break;
+        case 'F':
+            sex = 'F';
+            break;
+        case 'f':
+            sex = 'f';
+            break;
+        default:
+            cout << "Invalid entry!";
+            return 0;
+    }
+
     
-    }while(cin.fail()||(sex!='M')||(sex!='m')||(sex!='F')||(sex!='f'));
-
-
-
 
     return 0;
 }
