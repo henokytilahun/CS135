ff9468e614e585f21de6680954cba8bf8880b27c
henokytilahun <henokytilahun@gmail.com>
2022-10-01 17:28:54 -0700

    Changing Direction

--- Assignments/Assignment_3/main.cpp
+++ Assignments/Assignment_3/main.cpp
@@ -31,68 +31,86 @@ void heart_display()
 int main(){
 
     //initalize global variables
-    int age = 0;
-    int weight = 0;
-    int height = 0;
-    std::string sex = "";
+    int age = 10;
+    int weight = 50;
+    int height = 40;
+    char sex = 'M';
     int act_lvl = 0;
 
     //ask user for age
-    std::cout << "To get started, enter your age in years:\n";
+    cout << "To get started, enter your age in years:\n";
     do
     {
-      if(std::cin.fail())
+      if(cin.fail())
       {
-        std::cout << "Invalid Age!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Invalid Age!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       } else if (age < 10 || age > 79)
       {
-        std::cout << "Age must be between 10 and 79 years!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Age must be between 10 and 79 years!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       }
-      std::cin >> age;
-    } while (std::cin.fail() || age < 10 || age > 79);
+      cin >> age;
+    } while (cin.fail() || age < 10 || age > 79);
 
     //ask user for weight
-    std::cout << "Enter weight in lbs:\n";
+    cout << "Enter weight in lbs:\n";
     do
     {
-      if(std::cin.fail())
+      if(cin.fail())
       {
-        std::cout << "Invalid weight!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Invalid weight!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       } else if(weight < 50 || weight > 400)
       {
-        std::cout << "Weight must be between 50 lbs and 400 lbs!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Weight must be between 50 lbs and 400 lbs!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       }
-      std::cin >> weight;
-    } while (std::cin.fail() || weight < 50 || weight > 400);
+      cin >> weight;
+    } while (cin.fail() || weight < 50 || weight > 400);
 
 
     //ask user for height
-    std::cout << "Enter height in inches:\n";
+    cout << "Enter height in inches:\n";
 
     do
     {
-      if(std::cin.fail())
+      if(cin.fail())
       {
-        std::cout << "Invalid height!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Invalid height!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       } else if(height < 40 || height > 90)
       {
-        std::cout << "Heigh must be between 40\" and 90\"!\n";
-        std::cin.clear();
-        std::cin.ignore(100,'\n');
+        cout << "Heigh must be between 40\" and 90\"!\n";
+        cin.clear();
+        cin.ignore(100,'\n');
       }
-      std::cin >> height;
-    } while (std::cin.fail() || height < 40 || height > 90);
+      cin >> height;
+    } while (cin.fail() || height < 40 || height > 90);
 
+    //ask for user Gender
+    cout << "Enter 'M' for male or 'F' for female:\n";
+    do
+    {
+        if(cin.fail())
+        {
+            cout << "Invalid Entry!\n";
+            cin.clear();
+            cin.ignore(100,'\n');
+        } else if((sex!='M')||(sex!='m')||(sex!='F')||(sex!='f'))
+        {
+            cout << "Invalid Entry!\n";
+            cin.clear();
+            cin.ignore(100,'\n');
+        }
+        cin >> sex;
+    
+    }while(cin.fail()||(sex!='M')||(sex!='m')||(sex!='F')||(sex!='f'));
 
 
 
