9e7bf9cbe1ca8166302866117a0dac2a160fbfc3
henokytilahun <henokytilahun@gmail.com>
2022-10-13 13:34:35 -0700

    adding appending to file

--- Lec_Prac/main.cpp
+++ Lec_Prac/main.cpp
@@ -5,7 +5,7 @@ int main ()
     //1.create an ofstream object
     std::ofstream writer;
     //2.open or create a file
-    writer.open("out.txt");
+    writer.open("out.txt", std::fstream::app);
     //3.write to the file
     writer << "Hello World\n";
     //4.close the file
