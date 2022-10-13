8e97fed735c4c3686a8c81608f2ae9457a8c2645
henokytilahun <henokytilahun@gmail.com>
2022-10-13 14:00:27 -0700

    Final of reading example

--- Lec_Prac/FileIO/Files/main.cpp
+++ Lec_Prac/FileIO/Files/main.cpp
@@ -3,20 +3,35 @@
 
 int main()
 {
-    //1.create an ifstream object
+    // 1.create an ifstream object
     std::ifstream reader;
-    //2.Open a file
+    // 2.Open a file
     reader.open("in1.txt");
-    //2.1 Optionally, check if file was opened
-    //3.read the file
+    // 2.1 Optionally, check if file was opened
+    if(!reader)
+    {
+        std::cout << "error, opening file!\n";
+        return 0;
+    }
+    // 3.read the file
     std::string line;
-    //Not a step
+    // Not a step
+    /*
     while(std::getline(reader, line))
     {
         std::cout << line << std::endl;
     }
+    */
 
-    //4.close the file
-    reader.close();
+    //other way to read full file. This one can lead to infinite loop
+    while (!reader.eof())
+    {
+        std::getline(reader, line);
+        std::cout << line << std::endl;        
+    }
+    
+
+        // 4.close the file
+        reader.close();
     return 0;
 }
