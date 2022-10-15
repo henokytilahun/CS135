91919cd0c7fb6a5455a10cab95cb090f63a4f2ab
henokytilahun <henokytilahun@gmail.com>
2022-10-14 19:01:53 -0700

    Finally got the input failure to work :)

--- Lab_#5B/main.cpp
+++ Lab_#5B/main.cpp
@@ -3,7 +3,34 @@
 
 int main()
 {
+    std::ifstream reader;
+    std::string file_name;
+    int total, a, b = 0;
 
+    std::cout << "Enter file name\n**";
+    std::cin >> file_name;
+
+    reader.open(file_name);
+
+    if (!reader)
+    {
+        do
+        {
+            if (!reader)
+            {
+                std::cout << "Error: Invalid file name\n"
+                          << "Enter file name\n**";
+                std::cin.clear();
+                std::cin.ignore(100, '\n');
+            }
+            std::cin >> file_name;
+            reader.open(file_name);
+        } while (!reader);
+    }
+
+    
+
+    reader.close();
 
     return 0;
 }
