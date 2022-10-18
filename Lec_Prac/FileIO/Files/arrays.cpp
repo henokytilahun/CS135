abdc63d4dffea4e2e047e827da523a85c499b8fd
henokytilahun <henokytilahun@gmail.com>
2022-10-18 14:14:29 -0700

    Arrays w/ strings

--- Lec_Prac/FileIO/Files/arrays.cpp
+++ Lec_Prac/FileIO/Files/arrays.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-    int numbers[10] = {1, 2, 3, 4, 5, 6, 7 ,8};
-    for(int i = 0; i < 8; i++)
+    std::string friends[] = {"Henok", "Devin", "Sarah", "Julia", "Seth"};
+    for(int i = 0; i < 5; i++)
     {
-        std::cout << numbers[i] << std::endl;
+        std::cout << friends[i] << std::endl;
     }
     return 0;
 }
