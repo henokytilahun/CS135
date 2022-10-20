34d0f3eaa201ffe12f8f8aee5b1fe5dc5b0fd4ad
henokytilahun <henokytilahun@gmail.com>
2022-10-20 14:16:30 -0700

    How to add and remove in vectors continued

--- Lec_Prac/FileIO/Files/4main.cpp
+++ Lec_Prac/FileIO/Files/4main.cpp
@@ -7,6 +7,12 @@ int main()
 
     friends.push_back("Betti");
     friends.pop_back();
+    friends.insert(friends.begin(), "Jonatan");
+    friends.insert(friends.begin() + 1, "Joni");
+    friends.insert(friends.end() -1, "Matti");
+    friends.erase(friends.begin());
+    
+    std::cout << friends.size() << std::endl;
     for(std::string fri : friends)
     {
         std::cout << fri << "\n";
