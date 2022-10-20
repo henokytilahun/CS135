3dc28ba7a902a414d07f4c4e31158421319b4cce
henokytilahun <henokytilahun@gmail.com>
2022-10-20 14:12:42 -0700

    Adding and removing the last element in vector

--- Lec_Prac/FileIO/Files/4main.cpp
+++ Lec_Prac/FileIO/Files/4main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     std::vector<std::string> friends = {"Henok", "Amberber", "Ergetachew"};
 
+    friends.push_back("Betti");
+    friends.pop_back();
     for(std::string fri : friends)
     {
         std::cout << fri << "\n";
