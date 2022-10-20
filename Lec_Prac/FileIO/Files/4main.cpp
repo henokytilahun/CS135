62e6208a41838c1f9deea945ae451c9dcf31468a
henokytilahun <henokytilahun@gmail.com>
2022-10-20 14:05:52 -0700

    Swaping elements in arrays

--- Lec_Prac/FileIO/Files/4main.cpp
+++ Lec_Prac/FileIO/Files/4main.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-    int arr[] = {1,2,35,7,32,41,12,41,2,43,4,58,86,37,8,11,2,3,8,31,24,74,7,32,6};
-
-    std::cout <<"Memory consumed by the array: " << sizeof(arr) << " bytes\n";
-    std::cout << "Memory consumed by a cell: " << sizeof(arr[0]) << "bytes\n";
-    std::cout << "Num of Elements in array: " << sizeof(arr)/sizeof(arr[0]) << "\n";
-
-
+    double numbers[] = {1.15, 5.1};
+    double temp = numbers[0];
+    numbers[0] = numbers[1];
+    numbers [1] = temp;
     /*
-        int numbers[8];
-        for(int i = 0; i< sizeof(numbers/sizeof(numbers[i])); i++)
-        {
-            std::cout < numbers[i];
-        }
-    
+    for(datatype temp : arrName)
+    {
+        code
+    }
     */
+
+    for(double num : numbers)
+    {
+        std::cout << num << std::endl;
+    }
     return 0;
 }
