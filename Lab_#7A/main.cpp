3fcac29dd8ea1f958a7e97e9747213fda5bbd72c
henokytilahun <henokytilahun@gmail.com>
2022-10-24 17:38:23 -0700

    Got rows and columns in array perfectly

--- Lab_#7A/main.cpp
+++ Lab_#7A/main.cpp
@@ -6,7 +6,8 @@ int main()
 {
     std::string filename = "";
     std::ifstream reader;
-
+    int r = 7, c = 10;
+    double arr[r][c];
     std::cout << "Enter file name\n**";
     do
     {
@@ -16,14 +17,33 @@ int main()
         {
             if (!reader)
             {
-                std::cout << ""
-                    << "";
+                std::cout << "\nError: Invalid File Name\n"
+                    << "Enter file name\n**";
                 reader.clear();
                 reader.ignore(100, '\n');
             }
             reader.open(filename.c_str());
         }
     } while (!reader);
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            reader >> arr[i][j];
+        }
+    }
+
+    reader.close();
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
 
 
     return 0;
