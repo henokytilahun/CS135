1b17bd52582a4d76d5831d88a247fc6b1bc4aab2
henokytilahun <henokytilahun@gmail.com>
2022-10-25 13:47:45 -0700

    counting how many times char is in string

--- Lec_Prac/FileIO/Files/2vector.cpp
+++ Lec_Prac/FileIO/Files/2vector.cpp
@@ -6,12 +6,14 @@ int main ()
     std::string str = "hello World";
     char search = 'r';
     bool found = false;
+    int count = 0;
 
     for (char temp : str)
     {
         if(temp == search)
         {
             found = true;
+            count++;
         }
     }
 
