ca603158b4414955e9ccbd204b70705e57adc376
henokytilahun <henokytilahun@gmail.com>
2022-11-08 14:02:07 -0800

    default value ex 2

--- Lec_Prac/FileIO/Files/pow.cpp
+++ Lec_Prac/FileIO/Files/pow.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 using std::cout, std::string, std::endl;
+void printArray(int a[], int size, int startFrom = 0);
 
 double myPow (double base, double exp = 2)
 {
@@ -10,8 +11,18 @@ double myPow (double base, double exp = 2)
 
 int main()
 {
-    cout << myPow(2,5) << endl;
-    cout << myPow(5) << endl;
-    
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    printArray(arr,10);
+    printArray(arr,10,4);
+
     return 0;
+}
+
+void printArray(int a[], int size, int startFrom) //notice how there isn't a default value here
+{
+    for(int i = startFrom; i < size; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
